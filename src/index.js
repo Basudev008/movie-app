16a831db28cf8fe48457e914d78b974482e6c136
Basudev Singh <bassingh@bassingh-mn1.linkedin.biz>
2022-08-21 23:05:36 +0530

    complete movie app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore, applyMiddleware } from 'redux';
+import thunk  from 'redux-thunk';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import rootReducer from './reducers';
+
+// function logger (obj, next, action)
+// logger(obj)(next)(action)
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       // middleware code
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // logger code
+  if(typeof action !== 'function') {
+    console.log('ACTION_TYPE = ', action.type);
+  }
+  next(action);
+}
+
+// const thunk = ({ dispatch, getState }) => (next) => (action) => {
+//   // logger code
+//   if(typeof action === 'function') {
+//     action(dispatch);
+//     return;
+//   }
+//   next(action);
+// }
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+console.log('store', store);
+// console.log(store);
+// console.log('STATE', store.getState())
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'Superman'}]
+// });
+
+// console.log('After STATE', store.getState())
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
